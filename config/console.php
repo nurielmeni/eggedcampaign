944418e3aac2787f7c9bc1a59aa3764fa9df3b4c
Meni Nuriel <nurielmeni@gmail.com>
2021-12-04 11:33:20 +0200

    f

--- config/console.php
+++ config/console.php
@@ -38,10 +38,10 @@ $config = [
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
+    // $config['bootstrap'][] = 'gii';
+    // $config['modules']['gii'] = [
+    //     'class' => 'yii\gii\Module',
+    // ];
 }
 
 return $config;
