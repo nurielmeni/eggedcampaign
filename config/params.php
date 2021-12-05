d60c7985adc65b23929a54cca4d6fae1446a8f62
Meni Nuriel <nurielmeni@gmail.com>
2021-12-05 15:07:16 +0200

    params

--- config/params.php
+++ config/params.php
@@ -1,7 +1,5 @@
 <?php
-
 return [
-    'adminEmail' => 'admin@example.com',
     'datePickerKvOptions' => [
         'type' => 1,
         'readonly' => true,
@@ -10,7 +8,7 @@ return [
             'format' => 'dd/mm/yyyy',
             'disableTouchKeyboard' => true,
             'todayHighlight' => true,
-        ]
+        ],
     ],
     'supplierIdFbf' => '528e3917-7e8d-436d-9eb3-46570a63a42d',
     'supplierId' => '528e3917-7e8d-436d-9eb3-46570a63a42d',
@@ -18,5 +16,6 @@ return [
     'cvWebMailName' => 'Egged CV WebMail',
     'additionalJobs' => 'http://mobi.egged.co.il/%D7%9E%D7%90%D7%9E%D7%A8-14992-%D7%9E%D7%A9%D7%A8%D7%95%D7%AA-%D7%A0%D7%95%D7%A1%D7%A4%D7%95%D7%AA.aspx',
     'maxNumberOfJobs' => 1000,
-    'flushCache' => true
+    'flushCache' => 'true',
+    'bccMail' => 'nurielmeni@gmail.com',
 ];
